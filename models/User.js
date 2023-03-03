e413f10a47ba1fa9612176d67b006ede0e7d3776
Josh <lutz_143@yahoo.com>
2023-03-03 12:05:50 -0500

    updated User model for thoughts and friends reference

--- models/User.js
+++ models/User.js
@@ -16,6 +16,22 @@ const userSchema = new Schema(
       lowercase: true,
       validate: [validateEmail, 'Please add a valid email address'],
         match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
-    }
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref:'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref:'User'
+    }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
   }
-)
+);
+
+module.exports = userSchema;
