fed8ba2a489f0655e5c4f50897b217aca4783f24
Josh <lutz_143@yahoo.com>
2023-03-03 12:23:41 -0500

    added initial reactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -28,3 +28,31 @@ const thoughtSchema = new Schema(
     id: false
   }
 )
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,      
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+)
