aae969f8348d95777b60053e831f8694b6ab0793
Josh <lutz_143@yahoo.com>
2023-03-03 12:19:45 -0500

    added initial Thought Schema. Will need to create a dateFormat utils for creation timestamp

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,30 @@
 const { Schema, Types } = require('mongoose');
+// will need to create a utils possibly for a timestamp on query
 
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+      type: String,
+      required: true,      
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    },
+    id: false
+  }
+)
