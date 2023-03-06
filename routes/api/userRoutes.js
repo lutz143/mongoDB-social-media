451514b84c3a4ef50e5fff9a2f9597ea868d1896
Josh <lutz_143@yahoo.com>
2023-03-06 14:09:51 -0500

    added initial useRoutes.js

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/user/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
